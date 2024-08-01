09ebc06761bec639993f02ede95892b7a5bc41cf
Tulika <tulika.malik@metaverk.io>
2024-08-01 15:30:21 +0530

    apllication status handle for rejected case

--- src/Component/Common/ApplicationsList/MurabahaAgreement/ApplicationStatus.jsx
+++ src/Component/Common/ApplicationsList/MurabahaAgreement/ApplicationStatus.jsx
@@ -49,7 +49,7 @@ const statuses = ["AWAITING_DOCUMENT_EXTRACTION", "DOCUMENT_EXTRACTION_COMPLETED
       let payload = {
         ids: arr?.map((ele) => ele?._id),
         status: status || "AWAITING_COMMODITY_PURCHASE",
-        showStatus: "Pending",
+        showStatus: status === 'REJECTED'?  'REJECTED' : "Pending",
         portal_id: BASE_CONFIG?.APP_PORTAL_ID,
       };
 
@@ -64,7 +64,12 @@ const statuses = ["AWAITING_DOCUMENT_EXTRACTION", "DOCUMENT_EXTRACTION_COMPLETED
           APP_PLATFORM === "INTELLISCAN"
             ? "/admin/intelliscan-personal-finance-murbaha-details"
             : "/admin/application/list";
-        navigate(nvUrl);
+            if(status === 'REJECTED'){
+              navigate("/admin/intelliscan?status=fail");
+            }else {
+              navigate(nvUrl);
+            }
+       
       } else {
       }
     } catch (error) {
@@ -85,10 +90,10 @@ const statuses = ["AWAITING_DOCUMENT_EXTRACTION", "DOCUMENT_EXTRACTION_COMPLETED
    
 
     const isAllNo = (doc) =>
-      doc.application === "NO" &&
-      doc.credit_limit_approval === "NO" &&
-      doc.promise_to_purchase === "NO" &&
-      doc.status === "DOCUMENT_EXTRACTION_COMPLETED" &&
+      doc.application === "NO" ||
+      doc.credit_limit_approval === "NO" ||
+      doc.promise_to_purchase === "NO" ||
+      doc.status === "DOCUMENT_EXTRACTION_COMPLETED" ||
       (APP_PLATFORM !== "SO360"
         ? doc.murabaha_agreement === "NO" && doc.supporting_document === "NO"
         : true);
